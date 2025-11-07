5594da57e76fc97b337ccd01b256627d53a2cfea
sowmisasi0609 <sowmisasi0609@gmail.com>
2025-11-07 13:39:09 +0530

    commited

--- myproject/settings.py
+++ myproject/settings.py
@@ -104,13 +104,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static & Media
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'shop', 'static')]  
-STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'shop' / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
